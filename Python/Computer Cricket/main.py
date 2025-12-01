f310ba40168b1d8ffc9d65070ee3787c13c95163
Debdatta Mandal <debdattamandalin@users.noreply.github.com>
2025-12-01 23:03:28 +0530

    Fixed Rules If Else

--- Python/Computer Cricket/main.py
+++ Python/Computer Cricket/main.py
@@ -2,22 +2,64 @@ import random
 import time
 
 time.sleep(1)
-print("Hello player, welcome to computer cricket! \n")
 
-name = input("Before we begin, what is your name player? \n")
+print("Hello Player, Welcome To Computer Cricket! \n")
 
-print("Hello!", name, "nice to meet you! \n")
+name = input("Before We Begin, What Is Your Name Player? \n")
+
+print("Hello!", name, "Nice To Meet You! \n")
 
 while True:
-    htp = input("Do you want to know how to play this game? (Yes/No) \n")
+    dyk = input("Do You Know How To Play This Game? (Yes/No) \n")
 
-    if htp.lower() == "yes":
-        print("Cool! \n")
+    if dyk.lower() == "yes":
+
+        print("Good Then We Can Begin! \n")
         break
-    elif htp.lower() == "no":
-        print("You pressed no \n")
+
+    elif dyk.lower() == "no":
+
+        while True:
+            htp = input("Would You Like To Know How To Play This Game? (Yes/No)\n")
+
+            if htp.lower() == "yes":
+                print("How To Play")
+                break
+            elif htp.lower() == "no":
+                print("Okay Then Let's Begin Without The How To Play!")
+                break
+            else:
+                print("Wrong Input Please Try Again! \n")
+
+        break
+
+    else:
+        print("Wrong Input, Please Try Again! \n")
+
+toss = random.randint(1,2)
+
+while True:
+    tossh = input("Heads or Tails? (Head/Tail) \n")
+
+    if tossh.lower() == "head":
+        tossh = 1
+        break
+    elif htp.lower() == "tail":
+        tossh = 2
         break
     else:
-        print("Wrong Input, Please Try Again With (Yes/No). \n")
+        print("Wrong Input, Please Try Again. \n")
+
+if tossh == toss:
+    print("You won the toss!")
+else:
+    print("I won the toss")
+
+if tossh == toss:
+    print()
+else:
+    print()
+
+
 
 print("Worked! \n")
