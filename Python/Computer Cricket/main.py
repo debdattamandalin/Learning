df4ea5b68d1d4f5e0defbd3d75cb7397cb9647a8
Debdatta Mandal <debdattamandalin@users.noreply.github.com>
2025-12-01 22:20:05 +0530

    Added Computer Cricket

--- Python/Computer Cricket/main.py
+++ Python/Computer Cricket/main.py
@@ -2,4 +2,22 @@ import random
 import time
 
 time.sleep(1)
-print("Hello player, welcome to computer cricket!")
+print("Hello player, welcome to computer cricket! \n")
+
+name = input("Before we begin, what is your name player? \n")
+
+print("Hello!", name, "nice to meet you! \n")
+
+while True:
+    htp = input("Do you want to know how to play this game? (Yes/No) \n")
+
+    if htp.lower() == "yes":
+        print("Cool! \n")
+        break
+    elif htp.lower() == "no":
+        print("You pressed no \n")
+        break
+    else:
+        print("Wrong Input, Please Try Again With (Yes/No). \n")
+
+print("Worked! \n")
