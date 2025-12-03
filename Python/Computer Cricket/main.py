807404fe73c19b516744f410cc58010eb66d57e2
Debdatta Mandal <debdattamandalin@users.noreply.github.com>
2025-12-03 22:48:19 +0530

    Added some more things i am too confused what

--- Python/Computer Cricket/main.py
+++ Python/Computer Cricket/main.py
@@ -1,7 +1,9 @@
+#Imports
+
 import random
 import time
 
-time.sleep(1)
+#Player Introduction
 
 print("Hello Player, Welcome To Computer Cricket! \n")
 
@@ -9,18 +11,18 @@ name = input("Before We Begin, What Is Your Name Player? \n")
 
 print("Hello!", name, "Nice To Meet You! \n")
 
+#Do You Know And How To Play The Game
+
 while True:
     dyk = input("Do You Know How To Play This Game? (Yes/No) \n")
 
     if dyk.lower() == "yes":
-
         print("Good Then We Can Begin! \n")
         break
-
     elif dyk.lower() == "no":
 
         while True:
-            htp = input("Would You Like To Know How To Play This Game? (Yes/No)\n")
+            htp = input("Would You Like To Know How To Play This Game? (Yes/No) \n")
             if htp.lower() == "yes":
                 print("How To Play")
                 break
@@ -28,11 +30,13 @@ while True:
                 print("Okay Then Let's Begin Without The How To Play!")
                 break
             else:
-                print("Wrong Input Please Try Again! \n")
+                print("Invalid Input, Please Try Again! \n")
         break
 
     else:
-        print("Wrong Input, Please Try Again! \n")
+        print("Invalid Input, Please Try Again! \n")
+
+#Toss
 
 toss = random.randint(1,2)
 
@@ -46,27 +50,57 @@ while True:
         tossh = 2
         break
     else:
-        print("Wrong Input, Please Try Again. \n")
+        print("Invalid Input, Please Try Again! \n")
 
 if tossh == toss:
-    print("You Won The Toss!")
+
+    print("You Won The Toss! \n")
+
     while True:
         bob = input("Do You Want To Chose Batting Or Balling? (Bat/Ball) \n")
 
         if bob.lower() == "bat":
-            print("User Chose Batting")
+            print("User Chose Batting \n")
+            bob = 1
+            break
         elif bob.lower() == "ball":
-            print("User Chose Balling")
+            print("User Chose Balling \n")
+            bob = 2
+            break
         else:
-            print("Wrong Input Try Again!")
-else:
-    print("I won the toss")
+            print("Invalid Input, Please Try Again! \n")
 
-if tossh == toss:
-    print()
 else:
-    print()
+    print("I Won The Toss \n")
 
+bob = random.randint(1,2)
+
+if bob == 1:
+    print("I Am Choosing To Bat \n")
+else:
+    print("I Am Choosing To Ball \n")
+
+ball = int(input("Computer Threw The Ball, Your Attack? (Chose A Number Between 1 to 6) \n"))
+
+while True:
+    ball = int(input("Computer Threw The Ball, Your Attack? (Chose A Number Between 1 to 6) \n"))
+    balln = random.randint(1,6)
+    if ball == 1 or 2 or 3 or 4 or 5 or 6:
+        if ball == balln:
+            print("Out!")
+            scoreh = scoreh + 1
+            break
+        else:
+            print("Your Scored", ball,"\n")
+            scoreh = ball + scoreh
+    else:
+        print("Invalid Input")
+
+
+
+while True:
+    cv = random.randint(1,6)
+    hv = input("")
 
 
 print("Worked! \n")
